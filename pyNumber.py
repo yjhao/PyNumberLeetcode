0470d1c34f734e0fee75a53b75d5acfb3bab3340
Yujia <hyj143@gmail.com>
2016-03-19 18:54:26 -0400

    added: add, remove, check function; print the problems in ascending order

--- pyNumber.py
+++ pyNumber.py
@@ -1,6 +1,7 @@
 from random import randint
+import sys
 
-class pyNumber(object):
+class problemSet(object):
 
     def __init__ (self):
         self.list = set()
@@ -13,11 +14,12 @@ class pyNumber(object):
         lines = f.read().split(',')
         f.close()
         for item in lines:
-            self.addToSet(item)
+            if item != '':
+                self.addToSet(int(item))
 
     def saveToFile(self):
         with open("number.txt", "w") as myfile:
-            for item in self.list:
+            for item in sorted(self.list):
                 myfile.write(str(item))
                 myfile.write(",")
 
@@ -28,7 +30,7 @@ class pyNumber(object):
 
     def addToSet(self, val):
         if val not in self.list:
-            self.list.add(val)
+            self.list.add(int(val))
 
     def removeFromSet(self, val):
         if val in self.list:
@@ -50,32 +52,95 @@ class pyNumber(object):
             nextNumber = self.randomNumber()
         return nextNumber
 
+    def ifExist(self, val):
+        if val in self.list:
+            return True
+        return False
 
-### main Function
 
-def __main__():
-    py = pyNumber()
-    py.readFromFile()
+class pyNumber(object):
 
-    nextNumber = py.getNextNumber()
+    def __init__(self):
+        self.py = problemSet()
+        self.py.readFromFile()
 
-    print "\nNext question is:", nextNumber
+    def addOne(self):
+        if (len(sys.argv)>1):
+            first_arg = sys.argv[1]
+            second_arg = sys.argv[2]
+            addProblem = -1
 
-    nb = raw_input('Do you accept it?  Y(yes) or N(no)\n')
+            if (first_arg=="add"):
+                addProblem = int(second_arg)
+                if not self.py.ifExist(addProblem):
+                    self.py.addToSet(addProblem)
+                    self.py.saveToFile()
+                    print "\n Successfully add question", addProblem, "to your list!\n"
+                else:
+                    print "\n", addProblem, "already exists! Please add a different one!\n"
 
-    while nb!="y":
-        nextNumber = py.getNextNumber()
-        print "\nTry one more time. Next question is:", nextNumber
+                sys.exit(0)
+
+    def removeOne(self):
+        if (len(sys.argv)>1):
+            first_arg = sys.argv[1]
+            second_arg = sys.argv[2]
+            removeProblem = -1
+
+            if (first_arg=="remove"):
+                removeProblem = int(second_arg)
+                if self.py.ifExist(removeProblem):
+                    self.py.removeFromSet(removeProblem)
+                    print "\n Successfully remove question", removeProblem, "from your list!\n"
+                else:
+                    print "\n", removeProblem, "does not exist! Please choose a different one!\n"
+
+                sys.exit(0)
+
+    def check(self):
+        if (len(sys.argv)>1):
+            first_arg = sys.argv[1]
+            second_arg = sys.argv[2]
+
+            if (first_arg=="check"):
+                checkProblem = int(second_arg)
+                if self.py.ifExist(checkProblem):
+                    print "\n Found question", checkProblem, "from your list!\n"
+                else:
+                    print "\n", checkProblem, "does not exist\n"
+
+                sys.exit(0)
+
+
+
+
+    def getNext(self):
+        nextNumber = self.py.getNextNumber()
+
+        print "\nNext question is:", nextNumber
+
         nb = raw_input('Do you accept it?  Y(yes) or N(no)\n')
 
-    print "You accept the question ", nextNumber, "!\n\n"
-    py.addToSet(nextNumber)
-    py.appendToFile(nextNumber)
+        while nb!="y":
+            nextNumber = self.py.getNextNumber()
+            print "\nTry one more time. Next question is:", nextNumber
+            nb = raw_input('Do you accept it?  Y(yes) or N(no)\n')
 
+        print "You accept the question ", nextNumber, "!\n\n"
+        self.py.addToSet(nextNumber)
+        self.py.saveToFile()
+
+
+
+def __main__():
+    generator = pyNumber()
+    generator.addOne()
+    generator.removeOne()
+    generator.check()
+    generator.getNext()
 
 
 __main__()
-
 
 
 
